432cb572435919b8065003bce2faa1610ebed613
naveenkumardasari <147487898+23MHCQ05234@users.noreply.github.com>
2023-10-16 19:25:21 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d",&x,&y);      z=x+y;     printf("Sum:%d ",z);      z=x-y;     printf("Difference:%d ",z);      z=x*y;     printf("Product:%d ",z);      z=x/y;     printf("Quotient:%d ",z);      z=x%y;     printf("Remainder:%d ",z); }

---
+++ Arithmetic_operations.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+     z=x+y;
+    printf("Sum:%d
+",z);
+     z=x-y;
+    printf("Difference:%d
+",z);
+     z=x*y;
+    printf("Product:%d
+",z);
+     z=x/y;
+    printf("Quotient:%d
+",z);
+     z=x%y;
+    printf("Remainder:%d
+",z);
+}
