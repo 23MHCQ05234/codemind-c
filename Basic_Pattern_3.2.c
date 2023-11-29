f2a6b0e5efc4d50ae2d6e9979e36804ef98ceb7b
naveenkumardasari <147487898+23MHCQ05234@users.noreply.github.com>
2023-11-29 22:13:45 +0530

    #include<stdio.h> int main() {     int n,a,b;     scanf("%d",&n);     for(a=1;a<=n;a++)     {         for(b=1;b<=n-a+1;b++)         {             printf("%d ",a);         }         printf(" ");     } }

---
+++ Basic_Pattern_3.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b;
+    scanf("%d",&n);
+    for(a=1;a<=n;a++)
+    {
+        for(b=1;b<=n-a+1;b++)
+        {
+            printf("%d ",a);
+        }
+        printf("
+");
+    }
+}
