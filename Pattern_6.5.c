48828e53f0bdf9a5052b9445fac8e43e03eb8413
naveenkumardasari <147487898+23MHCQ05234@users.noreply.github.com>
2023-12-06 21:08:45 +0530

    #include<stdio.h> int main() {     int n,rows,cols,s;     scanf("%d",&n);     for(rows=1;rows<=n;rows++)     {         for(s=1;s<=rows-1;s++)         {             printf(" ");         }         for(cols=1;cols<=n-rows+1;cols++)         {             printf("%c ",rows+64);         }         printf(" ");     } }

---
+++ Pattern_6.5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,rows,cols,s;
+    scanf("%d",&n);
+    for(rows=1;rows<=n;rows++)
+    {
+        for(s=1;s<=rows-1;s++)
+        {
+            printf(" ");
+        }
+        for(cols=1;cols<=n-rows+1;cols++)
+        {
+            printf("%c ",rows+64);
+        }
+        printf("
+");
+    }
+}
