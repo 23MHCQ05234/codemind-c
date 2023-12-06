d9cd4fbe75d30f19560326f72346ae8575f88a08
naveenkumardasari <147487898+23MHCQ05234@users.noreply.github.com>
2023-12-06 22:40:32 +0530

    #include<stdio.h> int main() {     int n,a,b,s;     scanf("%d",&n);     for(a=1;a<=n;a++)     {         for(s=1;s<=a-1;s++)         {             printf(" ");         }         for(b=1;b<=n-a+1;b++)         {             printf("%c ",n-a+1+64);         }         printf(" ");     } }

---
+++ Pattern_7.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,s;
+    scanf("%d",&n);
+    for(a=1;a<=n;a++)
+    {
+        for(s=1;s<=a-1;s++)
+        {
+            printf(" ");
+        }
+        for(b=1;b<=n-a+1;b++)
+        {
+            printf("%c ",n-a+1+64);
+        }
+        printf("
+");
+    }
+}
